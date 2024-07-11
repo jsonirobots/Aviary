166c103127b4aa5ba967c83b8a658a2a21d80381
Jatin Soni <jatinsoni006@gmail.com>
2024-07-11 14:37:45 -0400

    checkpoint

--- aviaryPlane.py
+++ aviaryPlane.py
@@ -15,49 +15,75 @@ from bypassGUI import bypassGUI
 # throttle enforcement options: 'path_constraint', 'boundary_constraint', 'bounded', None
 
 # use provided plane name in cli to locate csv and find mission trajectory points
+options = []
 if len(sys.argv) > 1:
     planename = sys.argv[1]
     missions,plane_file = bypassGUI(planename)
     if len(sys.argv) > 2:
-        option = sys.argv[2]
+        options = sys.argv[2:]
 
 else:
     raise Exception("No airplane name entered! E.g. python aviaryPlane.py planeName")
 
-for key,mission in missions.items():
-    phase_info = mission["phase_info"]
-    mission["prob_name"] = f"{planename}_{key}"
-    mission["prob"] = av.run_aviary(plane_file, phase_info,
-                        optimizer="SLSQP", make_plots=True,max_iter = 50,prob_name=mission["prob_name"])
+if not "rangecheck" in options:
+    for key,mission in missions.items():
+        phase_info = mission["phase_info"]
+        with open(f"planeCSVs/{planename}.csv","r") as fp:
+            with open("planeCSVs/tempav.csv","w") as fd:
+                lines = fp.readlines()
+                for i,line in enumerate(lines):
+                    # if "aircraft:design:empty_mass" in line:
+                    #     lines[i] = "# "+line
+                    if "aircraft:crew_and_payload:cargo_mass" in line:
+                        if key == "ferry":
+                            lines[i] = line.split(",")[0]+",0,"+line.split(",")[2]
+                        elif key == "morepayload":
+                            lines[i] = line.split(",")[0]+",164900,"+line.split(",")[2]
+                    
+                fd.writelines(lines)
 
-for key,mission in missions.items():
-    with open("reports/"+mission["prob_name"]+"/mission_summary.md","r") as report:
-        for line in report:
-            if "Total Ground Distance" in line:
-                range = float(line.split("|")[2])
+        mission["prob_name"] = f"{planename}_{key}"
+        mission["prob"] = av.run_aviary("planeCSVs/tempav.csv", phase_info,
+                            optimizer="SLSQP", make_plots=True,max_iter = 50,prob_name=mission["prob_name"])
 
-    df = pd.read_csv("reports/"+mission["prob_name"]+"/mission_timeseries_data.csv")
-    firstthr = df['throttle (unitless)'][0]
-    firstdrag = df['drag (lbf)'][0]
-    prob = mission["prob"]
+    printdata = []
+    for key,mission in missions.items():
+        with open("reports/"+mission["prob_name"]+"/mission_summary.md","r") as report:
+            for line in report:
+                if "Total Ground Distance" in line:
+                    range = float(line.split("|")[2])
 
-    print("=========================================================")
-    print(f"-------------- Problem Name: {mission['prob_name']} -----")
-    print(f"                Total range: {range:10.2f} nmi")
-    print(f"       First throttle point: {firstthr:10.2f}")
-    print(f"           First drag point: {firstdrag:10.2f} lbf")
-    print(f"                 Gross mass: {prob.get_val(av.Mission.Design.GROSS_MASS,units='lbm')[0]:10.2f} lbm")
-    print(f"                 Empty mass: {prob.get_val(av.Aircraft.Design.EMPTY_MASS,units='lbm')[0]:10.2f} lbm")
-    print(f"          Total wetted area: {prob.get_val(av.Aircraft.Design.TOTAL_WETTED_AREA,units='ft**2')[0]:10.2f} sqft")
-    print(f"           Wing wetted area: {prob.get_val(av.Aircraft.Wing.WETTED_AREA,units='ft**2')[0]:10.2f} sqft")
-    print(f"       Fuselage wetted area: {prob.get_val(av.Aircraft.Fuselage.WETTED_AREA,units='ft**2')[0]:10.2f} sqft")
-    print(f"Horizontal Tail wetted area: {prob.get_val(av.Aircraft.HorizontalTail.WETTED_AREA,units='ft**2')[0]:10.2f} sqft")
-    print(f"  Vertical Tail wetted area: {prob.get_val(av.Aircraft.VerticalTail.WETTED_AREA,units='ft**2')[0]:10.2f} sqft")
-    print(f"        Nacelle wetted area: {prob.get_val(av.Aircraft.Nacelle.WETTED_AREA,units='ft**2')[0]:10.2f} sqft")
+        df = pd.read_csv("reports/"+mission["prob_name"]+"/mission_timeseries_data.csv")
+        firstthr = df['throttle (unitless)'][0]
+        firstdrag = df['drag (lbf)'][0]
+        prob = mission["prob"]
 
-    if len(sys.argv) > 2:
-        if option == "dashboard":
-            os.system("aviary dashboard "+planename)
+        vals = [range,firstthr,firstdrag,
+                prob.get_val(av.Mission.Design.GROSS_MASS,units='lbm')[0],
+                prob.get_val(av.Aircraft.Design.EMPTY_MASS,units='lbm')[0],
+                prob.get_val(av.Aircraft.Design.TOTAL_WETTED_AREA,units='ft**2')[0],
+                prob.get_val(av.Aircraft.Wing.WETTED_AREA,units='ft**2')[0],
+                prob.get_val(av.Aircraft.Fuselage.WETTED_AREA,units='ft**2')[0],
+                prob.get_val(av.Aircraft.HorizontalTail.WETTED_AREA,units='ft**2')[0],
+                prob.get_val(av.Aircraft.VerticalTail.WETTED_AREA,units='ft**2')[0],
+                prob.get_val(av.Aircraft.Nacelle.WETTED_AREA,units='ft**2')[0]]
+        printdata.append(vals)
+
+    print("=====================================================================")
+    units = ["nmi","","lbf","lbm","lbm","sqft","sqft","sqft","sqft","sqft","sqft"]
+    labels = ["Total range","First throttle point","First drag point","Gross mass","Empty mass",
+              "Total wetted area","Wing wetted area","Fuselage wetted area","Horizontal Tail wetted area",
+              "Vertical Tail wetted area","Nacelle wetted area"]
+    for row,(label,unit) in enumerate(zip(labels,units)):
+        if row == 0:
+            titlestuff = ' | '.join([f"{mission['prob_name']:>16}" for mission in missions.values()])
+            print(f"{'Problem Name':>30}: {titlestuff}")
+            print("---------------------------------------------------------------------")
+        numstuff = ' | '.join([f"{printdata[col][row]:10.2f} {unit:5s}" for col,_ in enumerate(printdata)])
+        print(f"{labels[row]:>30}: {numstuff}")
+
+    if "dashboard" in options:
+        os.system("aviary dashboard "+planename)
 
 # ----------
 # extra code that may be useful
@@ -84,3 +110,18 @@ for key,mission in missions.items():
 
 # # Add the reserve phase to phase_info
 # phase_info_res.update({'reserve_cruise': reserve_phase_0})
+
+
+        # print("=========================================================")
+        # print(f"-------------- Problem Name: {mission['prob_name']} -----")
+        # print(f"                Total range: {range:10.2f} nmi")
+        # print(f"       First throttle point: {firstthr:10.2f}")
+        # print(f"           First drag point: {firstdrag:10.2f} lbf")
+        # print(f"                 Gross mass: {prob.get_val(av.Mission.Design.GROSS_MASS,units='lbm')[0]:10.2f} lbm")
+        # print(f"                 Empty mass: {prob.get_val(av.Aircraft.Design.EMPTY_MASS,units='lbm')[0]:10.2f} lbm")
+        # print(f"          Total wetted area: {prob.get_val(av.Aircraft.Design.TOTAL_WETTED_AREA,units='ft**2')[0]:10.2f} sqft")
+        # print(f"           Wing wetted area: {prob.get_val(av.Aircraft.Wing.WETTED_AREA,units='ft**2')[0]:10.2f} sqft")
+        # print(f"       Fuselage wetted area: {prob.get_val(av.Aircraft.Fuselage.WETTED_AREA,units='ft**2')[0]:10.2f} sqft")
+        # print(f"Horizontal Tail wetted area: {prob.get_val(av.Aircraft.HorizontalTail.WETTED_AREA,units='ft**2')[0]:10.2f} sqft")
+        # print(f"  Vertical Tail wetted area: {prob.get_val(av.Aircraft.VerticalTail.WETTED_AREA,units='ft**2')[0]:10.2f} sqft")
+        # print(f"        Nacelle wetted area: {prob.get_val(av.Aircraft.Nacelle.WETTED_AREA,units='ft**2')[0]:10.2f} sqft")
